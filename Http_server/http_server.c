b623079c9873695a616cdca80f85ba0ad7cc7b8b
mengerya <30553694+mengerya@users.noreply.github.com>
2018-07-05 15:44:05 +0800

    Update http_server.c

--- Http_server/http_server.c
+++ Http_server/http_server.c
@@ -8,10 +8,38 @@
 #include<arpa/inet.h>
 #include<sys/wait.h>
 
+#define SIZE 10240
+
 typedef struct Arg{
 	int fd;
 	sockaddr_in addr;
 }Arg;
+
+typedef struct Request{
+
+	char first_line[SIZE];
+	char *method;
+	char *url_path;
+	char *query_string;
+	//char *version;
+	//接下来是header部分，如果要完整的解析下来，此处需要hash表，或二叉搜索树
+	//偷懒：不要其他header,只保留一个content_length
+	//content_length是一个整数，用int就可以了
+	int content_length;
+}Request;
+
+void* HttpServer(void* ptr){
+	Arg* arg = (Arg*)ptr;
+	int fd = arg->fd;
+	sockaddr_in addr=arg->addr;
+	//解析首行
+	//对首行进行解析（解析出方法，url,url_path,query_string）
+	
+	
+	//对header进行解析（只保留了Content_Length）
+	//对于静态页面，根据url_path,打开对应的文件，根据文件内容构造HTTP响应就可以了
+	//对于动态页面，按照CGI的规则来生成动态页面
+}
 
 void tcp_inio(char * ip,int port){
         //套接字
@@ -37,6 +65,8 @@ void tcp_inio(char * ip,int port){
 		perror("listen");
 		return;
 	}
+
+	printf("Server Inio OK\n");
 
 	while(1){
 		struct sockaddr_in new_addr;
